c7ba9e9d94cd9ab660a547f9de4fe1b20cf2d51b
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-10-14 19:45:12 -0300

    First commit

---
+++ src/test/java/com/eduardoguedes/confeitaria/ConfeitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.confeitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
